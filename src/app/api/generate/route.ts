818953cfd33968cb2cd495fe22cc70813d698cbf
Menushka Weeratunga <tmweeratunga@gmail.com>
2023-06-19 23:41:03 -0400

    Add 1 day cache

--- src/app/api/generate/route.ts
+++ src/app/api/generate/route.ts
@@ -2,6 +2,7 @@ import { prompt } from '@/data/prompt'
 import { Configuration, OpenAIApi } from 'openai-edge'
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import { NextResponse } from 'next/server'
+import kv from '@vercel/kv'
 
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
@@ -14,6 +15,11 @@ export async function POST(request: Request) {
   const { prompt: context } = await request.json()
 
   try {
+    const cached = await kv.get(context)
+    if (cached) {
+      return new NextResponse(cached as ReadableStream)
+    }
+
     const response = await openai.createCompletion({
       model: 'text-davinci-003',
       stream: true,
@@ -24,7 +30,13 @@ export async function POST(request: Request) {
       presence_penalty: 0.1,
       prompt: prompt(context)
     })
-    const stream = OpenAIStream(response)
+
+    const stream = OpenAIStream(response, {
+      async onCompletion(completion) {
+        await kv.set(context, completion)
+        await kv.expire(context, 24 * 60 * 60)
+      }
+    })
     return new StreamingTextResponse(stream)
   } catch (err) {
     console.error(err)
