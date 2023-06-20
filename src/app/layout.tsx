e2fc7681dd67a5addda8b2928163bb7b69db5f3b
Menushka Weeratunga <tmweeratunga@gmail.com>
2023-06-20 01:13:16 -0400

    Try with head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,36 @@ import { cookies } from 'next/headers'
 import { Inter } from 'next/font/google'
 import { THEME_KEY, Theme } from '@/data/theme'
 import clsx from 'clsx'
+import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const title = 'JokeGPT'
+const description = 'Your number one source for original, ai-plagiarized humor!'
+
+export const metadata: Metadata = {
+  title,
+  description,
+  keywords: ['Joke', 'GPT', 'AI'],
+  authors: [{ name: 'Menushka Weeratunga', url: 'https://menushka.ca' }],
+  metadataBase: new URL('https://jokegpt.io'),
+  openGraph: {
+    title,
+    description,
+    url: '/',
+    siteName: 'JokeGPT',
+    images: '/screenshot.png',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    creator: '@menushkaDev',
+    images: '/screenshot.png',
+  },
+}
 
 export default function RootLayout({
   children,
@@ -15,6 +43,7 @@ export default function RootLayout({
   const isDarkMode = cookieStore.get(THEME_KEY)?.value === Theme.Dark
   return (
     <html lang="en" className={clsx(isDarkMode && 'dark')}>
+      <head />
       <body className={inter.className}>{children}</body>
     </html>
   )
