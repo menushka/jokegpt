30a3d1584da75579067f4b35f88cefda1a25d63d
Menushka Weeratunga <tmweeratunga@gmail.com>
2023-06-20 00:42:02 -0400

    Fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,35 @@ import { cookies } from 'next/headers'
 import { Inter } from 'next/font/google'
 import { THEME_KEY, Theme } from '@/data/theme'
 import clsx from 'clsx'
+import type { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'JokeGPT',
-  description: 'Your number one source for original, ai-plagiarized humor!',
+const title = 'JokeGPT'
+const description = 'Your number one source for original, ai-plagiarized humor!'
+
+export const metadata: Metadata = {
+  title,
+  description,
+  keywords: ['Joke', 'GPT', 'AI'],
+  authors: [{ name: 'Menushka Weeratunga', url: 'https://menushka.ca' }],
+  metadataBase: new URL('https://jokegpt.io'),
+  openGraph: {
+    title,
+    description,
+    url: '/',
+    siteName: 'JokeGPT',
+    images: '/screenshot.png',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    creator: '@menushkaDev',
+    images: '/screenshot.png',
+  },
 }
 
 export default function RootLayout({
@@ -20,33 +43,6 @@ export default function RootLayout({
   const isDarkMode = cookieStore.get(THEME_KEY)?.value === Theme.Dark
   return (
     <html lang="en" className={clsx(isDarkMode && 'dark')}>
-      <head>
-        <title>{metadata.title}</title>
-
-        {/* Basic meta tags */}
-        <meta charSet="UTF-8" />
-        <meta name="description" content={metadata.description} />
-        <meta name="author" content="Menushka Weeratunga" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-        {/* Open Graph protocol (OGP) meta tags - for social media previews */}
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://jokegpt.io" />
-        <meta property="og:image" content="https://jokegpt.io/screenshot.png" />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:site_name" content={metadata.title} />
-
-        {/* Twitter card meta tags */}
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@menushkaDev" />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content="https://jokegpt.io/screenshot.png" />
-
-        {/* Other important meta tags */}
-        <link rel="canonical" href="https://jokegpt.io" />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-import { metadata } from './layout'
 import { Results } from '@/components/views/Results'
 import { Github, Moon, Sun, Twitter } from 'lucide-react'
 import { DarkModeSwitch } from '@/components/views/DarkModeSwitch'
 import Link from 'next/link'
 
+// Copied from src/app/layout.tsx
+const title = 'JokeGPT'
+const description = 'Your number one source for original, ai-plagiarized humor!'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-center items-center p-8">
-      <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center'>{metadata.title}</h1>
-      <h2 className='scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-center'>{metadata.description}</h2>
+      <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center'>{title}</h1>
+      <h2 className='scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 text-center'>{description}</h2>
       <Results />
       <div className='absolute top-0 left-0 w-full flex justify-between items-center gap-4 p-4'>
         <div className='flex gap-2'>
