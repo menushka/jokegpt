240285442adcf8b0888ddbd664da7b3cc71059a9
Menushka Weeratunga <tmweeratunga@gmail.com>
2023-06-20 00:18:37 -0400

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,33 @@ export default function RootLayout({
   const isDarkMode = cookieStore.get(THEME_KEY)?.value === Theme.Dark
   return (
     <html lang="en" className={clsx(isDarkMode && 'dark')}>
+      <head>
+        <title>{metadata.title}</title>
+
+        {/* Basic meta tags */}
+        <meta charSet="UTF-8" />
+        <meta name="description" content={metadata.description} />
+        <meta name="author" content="Menushka Weeratunga" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        {/* Open Graph protocol (OGP) meta tags - for social media previews */}
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://jokegpt.io" />
+        <meta property="og:image" content="https://jokegpt.io/screenshot.png" />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:site_name" content={metadata.title} />
+
+        {/* Twitter card meta tags */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@menushkaDev" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content="https://jokegpt.io/screenshot.png" />
+
+        {/* Other important meta tags */}
+        <link rel="canonical" href="https://jokegpt.io" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
